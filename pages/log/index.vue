b7aed38c2cbd52b5dc1595f18cd15ad9d12f6bed
fauzi8 <fauzi@gramedia.id>
2021-06-22 19:00:37 +0700

    add date slider, add revision homepage and log

--- pages/log/index.vue
+++ pages/log/index.vue
@@ -6,34 +6,42 @@
       <v-container>
         <v-row class="d-flex justify-end mb-5">
           <v-col cols="2">
-            <p class="mb-0 mt-1 text-right">Select Date</p>
+            <p class="mb-0 mt-1 text-right musa_green_dark_text">Select Date</p>
           </v-col>
           <v-col cols="2">
             <v-menu
+              content-class="date_single_range"
               ref="menu"
               v-model="menu"
               :close-on-content-click="false"
               :return-value.sync="date"
               transition="scale-transition"
-              offset-y
               min-width="auto"
             >
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
-                  class="mt-0 pt-0"
+                  class="mt-0 pt-0 date_range"
                   v-model="date"
                   readonly
                   v-bind="attrs"
                   v-on="on"
                   hide-details="auto"
+                  solo
+                  append-icon="mdi-menu-down"
                 ></v-text-field>
               </template>
-              <v-date-picker v-model="date" @change="dateChange(date)" no-title scrollable>
+              <v-date-picker
+                v-model="date"
+                @change="dateChange(date)"
+                no-title
+                scrollable
+                color="color_current_date"
+              >
                 <v-spacer></v-spacer>
-                <v-btn text color="primary" @click="menu = false">
+                <v-btn text @click="menu = false">
                   Cancel
                 </v-btn>
-                <v-btn text color="primary" @click="$refs.menu.save(date)">
+                <v-btn text @click="$refs.menu.save(date)">
                   OK
                 </v-btn>
               </v-date-picker>
@@ -68,7 +76,6 @@
                 :headers="headers"
                 :items="items"
                 :items-per-page="10"
-                class="elevation-1"
               ></v-data-table>
             </v-col>
           </v-row>
@@ -79,24 +86,29 @@
 </template>
 
 <script>
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 
-const QUERY_LOGS = gql `
-  query logs ($startTime: Timestamp, $endTime: Timestamp, $type: [String!], $level: [String!]) {
-    logs (startTime: $startTime, endTime: $endTime, type: $type, level: $level) {
-        timestamp
-        caller
-        level
-        message
-        type
+const QUERY_LOGS = gql`
+  query logs(
+    $startTime: Timestamp
+    $endTime: Timestamp
+    $type: [String!]
+    $level: [String!]
+  ) {
+    logs(startTime: $startTime, endTime: $endTime, type: $type, level: $level) {
+      timestamp
+      caller
+      level
+      message
+      type
     }
     todayLogSummary {
-        type
-        warn
-        error
+      type
+      warn
+      error
     }
   }
-`
+`;
 export default {
   name: "Log",
   data: () => ({
@@ -114,28 +126,26 @@ export default {
     loading: false
   }),
   mounted() {
-    this.getLogs()
-    console.log(this.date)
+    this.getLogs();
+    console.log(this.date);
   },
-  methods : {
+  methods: {
     async getLogs() {
       try {
-        this.loading = true
+        this.loading = true;
         const res = await this.$apollo.query({
           query: QUERY_LOGS,
           variables: {
-            "startTime": null,
-            "endTime": null,
-            "type": [
-            ],
-            "level": [
-            ]
-          },
+            startTime: null,
+            endTime: null,
+            type: [],
+            level: []
+          }
         });
 
         if (res) {
-          this.loading = false
-          let result = res.data.logs
+          this.loading = false;
+          let result = res.data.logs;
 
           result.map(items => {
             this.items.push({
@@ -143,17 +153,17 @@ export default {
               type: items.type,
               level: items.level,
               desc: items.message
-            })
-          })
+            });
+          });
         }
       } catch (err) {
-        console.log(err)
-        this.loading = false
+        console.log(err);
+        this.loading = false;
         // this.searchResults = [];
       }
     },
-    dateChange(val){
-      console.log(val)
+    dateChange(val) {
+      console.log(val);
     }
   }
 };
